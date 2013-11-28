19c17b1e6b7cbc403ec8c4f2efcea2a1095226f6
yoye <y.aparici@gmail.com>
2013-11-28 10:19:22 +0100

    Fix typo

--- Message/PaymentCardRequest.php
+++ Message/PaymentCardRequest.php
@@ -23,7 +23,7 @@ class PaymentCardRequest extends BaseRequest
             ->setOptional(array(
                 'Tag', 'TemplateURL',
                 'OwnerID', 'ReturnURL',
-                'culture',
+                'Culture',
             ))
         ;
 
